9a12a00aea271537939a6a60de0c3e521877c7b9
Ramiro Castro <ramirocastrocavalcanti@outlook.com>
2022-09-02 11:37:46 -0300

    Requisito 2 - função getAnimalsOlderThan concluida

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -2,6 +2,15 @@ const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
+  const obj = data.species.find((specie) => specie.name === animal); // por qual motivo vira um array de array se eu colodar array[index]? pq filter cria um array a cada valor id equivalente
+  // console.log(obj);
+  const maisVelho = obj.residents.filter((resident) => resident.age > age);
+  // console.log(maisVelho);
+  // console.log(obj.residents.length);
+  if (maisVelho.length !== obj.residents.length) {
+    return false; // se pelo menos um animal estiver menor ou maior que a idade passada em age
+  }
+  return true; // se todos os animais estiverem menores ou maiores que a idade passada em age
 }
-
+// console.log(getAnimalsOlderThan('otters', 7));
 module.exports = getAnimalsOlderThan;
