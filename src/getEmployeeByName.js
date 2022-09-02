a23759aa58a9176e892befdce41f52dd1b9bc836
Ramiro Castro <ramirocastrocavalcanti@outlook.com>
2022-09-02 03:26:57 -0300

    Requisito 3 - função getEmployeeByName concluida

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -2,6 +2,20 @@ const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
   // seu código aqui
+  let obj = {};
+  if (employeeName === undefined) {
+    return obj;
+  }
+
+  obj = data.employees.find((employe) => employe.firstName === employeeName);
+  if (obj === undefined) {
+    obj = data.employees.find((employe) => employe.lastName === employeeName);
+    return obj;
+  }
+
+  return obj;
 }
+
+// console.log(getEmployeeByName('Elser'));
 
 module.exports = getEmployeeByName;
