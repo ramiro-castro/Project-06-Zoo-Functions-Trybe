a5e6f533e93f094d5fbc8818c810309beda6baa2
Ramiro Castro <ramirocastrocavalcanti@outlook.com>
2022-09-04 12:43:24 -0300

    requisito 6 - handlerElephantsTest concluido

--- src/handlerElephants.js
+++ src/handlerElephants.js
@@ -3,7 +3,7 @@ const { species } = require('../data/zoo_data');
 const getElephants = () =>
   species.find((specie) => specie.name === 'elephants');
 
-const averageAge = ({ residents }) =>
+const averageAge = ({ residents }) =>// preciso ver uma outra implementacao para comprarar
   residents.reduce((sum, elephant) => sum + elephant.age, 0) / residents.length;
 
 const computeData = (param, elephants) => {
@@ -13,7 +13,8 @@ const computeData = (param, elephants) => {
   case 'names':
     return elephants.residents.map((elephant) => elephant.name);
   case 'averageAge':
-    return averageAge(elephants);
+    // console.log(typeof averageAge(elephants));
+    return averageAge(elephants); // envia para averAge o parameto elephants... tal parameto ja e linkado com sua key residents na funcao average
   default:
     return null;
   }
@@ -27,10 +28,13 @@ const handlerElephants = (param) => {
     return 'Parâmetro inválido, é necessário uma string';
   }
   const elephants = getElephants();
-  if (Object.keys(elephants).includes(param)) {
+  // console.log(Object.keys(elephants).includes('name'));
+  if (Object.keys(elephants).includes(param)) { // retorna valores presentes no objeto elephantes como nome, populacao maxima...
     return elephants[param];
   }
   return computeData(param, elephants);
 };
+
+// console.log(handlerElephants('averageAge'));
 
 module.exports = handlerElephants;
--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,42 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  it('retorna a quantidade de elephants', () => {
+    const actual = handlerElephants('count');
+    const expected = 4;
+    expect(actual).toBe(expected);
+  });
+  it('retorna algum valor do objeto elephants', () => {
+    {
+      const actual = handlerElephants('name');
+      const expected = 'elephants';
+      expect(actual).toBe(expected);
+    }
+
+    {
+      const actual = handlerElephants('education');
+      const expected = null;
+      expect(actual).toBe(expected);
+    }
+  });
+  it('retorna os nomes dos elephants', () => {
+    const actual = handlerElephants('names');
+    const expected = ['Ilana', 'Orval', 'Bea', 'Jefferson'];
+    expect(actual).toEqual(expected);
+  });
+  it('retorna a media de idade (average) dos elephants', () => {
+    const actual = handlerElephants('averageAge');
+    const expected = 10.5;
+    expect(actual).toEqual(expected);
+  });
+  it('retorna undefined caso nao tenha sido passado um parametro', () => {
+    const actual = handlerElephants();
+    const expected = undefined;
+    expect(actual).toBe(expected);
+  });
+  it('retorna aviso caso nao tenha sido passado um parametro do tipo string', () => {
+    const actual = handlerElephants(1993);
+    const expected = 'Parâmetro inválido, é necessário uma string';
+    expect(actual).toBe(expected);
+  });
+});
