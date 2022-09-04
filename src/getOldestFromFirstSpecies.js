5fbda178f1da3052498206372c5d2b5cb58eba96
Ramiro Castro <ramirocastrocavalcanti@outlook.com>
2022-09-04 20:32:45 -0300

    requisito 10 - getOldestFromFirstSpecies concluido

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -2,6 +2,18 @@ const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
   // seu código aqui
-}
+  const objEmploye = data.employees.find((employe) => employe.id === id); // por qual motivo vira um array de array se eu colodar array[index]? pq filter cria um array a cada valor id equivalente
+  const objFirstAnimal = objEmploye.responsibleFor.find((e) => e);
+  const objDadosAnimal = data.species.find((specie) => specie.id === objFirstAnimal);
+  const objResidents = objDadosAnimal.residents.map((resident) => resident);
+  const velho = objResidents.reduce((prev, current) => (prev.age > current.age ? prev : current)); // https://receitasdecodigo.com.br/front-end/encontrar-maior-ou-menor-valor-de-um-array-javascript#:~:text=Encontrar%20o%20maior%20valor%20de%20um%20array%20de%20objetos%20em%20Javasctipt&text=Considerando%20este%20array%20de%20objetos,%2C%20current)%20%7B%20return%20prev.
 
+  //   console.log(objFirstAnimal);
+  //   console.log(objDadosAnimal);
+  //   console.log(objResidents);
+  //   console.log(velho);
+  return Object.values(velho);
+}
+const burlId = '0e7b460e-acf4-4e17-bcb3-ee472265db83';
+console.log(getOldestFromFirstSpecies(burlId));
 module.exports = getOldestFromFirstSpecies;
