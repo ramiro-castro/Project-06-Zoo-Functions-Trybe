70f4f51e18072b7de28d6bf79d3bddf8b391b3e8
Ramiro Castro <ramirocastrocavalcanti@outlook.com>
2022-09-08 11:04:30 -0300

    requisito 14 - getAnimalMap concluido

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,137 @@
 const data = require('../data/zoo_data');
 
-function getAnimalMap(options) {
-  // seu código aqui
+function animalLocation(local) {
+  // console.log(idAnimal);
+  const array = [];
+  const animals = data.species.map((specie) => specie);
+  animals.forEach((animal, index) => {
+    if (animal.location === local) {
+      array.push(animal.name);
+    }
+  });
+  return array;
 }
 
+function comparaNomeSex(animal, sexo) {
+  const result = [];
+  animal.residents.forEach((resident) => {
+    if (resident.sex === sexo) {
+      result.push(resident.name);
+    }
+  });
+  return result;
+}
+
+function animalSex(animal, option) {
+  let sexo = 'male';
+  let nameSex = [];
+  if (option[2] !== undefined || option[4] !== undefined) {
+    sexo = 'female';
+  }
+
+  nameSex = comparaNomeSex(animal, sexo);
+
+  if (option[4] !== undefined || option[5] !== undefined) {
+    return nameSex.sort();
+  }
+  return nameSex;
+}
+
+function animalObject(animal, option) { // sort estara ou nao em option
+  const obj = {};
+  if (option[1] !== undefined) {
+    // console.log('entrei sera organizado em ordem alfabetica');
+    obj[animal.name] = (animal.residents.map((resident) => resident.name)).sort();// sera organizado em ordem alfabetica
+    // console.log(obj);
+    return obj;
+  }
+  for (let index = 2; index < option.length; index += 1) {
+    if (option[index] !== undefined) {
+    // console.log('entrei sera organizado em ordem alfabetica');
+      obj[animal.name] = animalSex(animal, option);
+      console.log(obj);
+      return obj;
+    }
+  }
+  obj[animal.name] = animal.residents.map((resident) => resident.name);
+  return obj;
+}
+
+function animalName(local, option) {
+  // console.log(idAnimal);
+  const arr = [];
+  let auxArr = 0;
+  const animals = data.species.map((specie) => specie);
+  animals.forEach((animal, index) => {
+    if (animal.location === local) { // OBS: tem que ser um array de obj
+      arr[auxArr] = animalObject(animal, option);
+      auxArr += 1;
+    }
+  });
+  // console.log(arr);
+  return arr;
+}
+
+function criaObjLocation(op) {
+  const objLocation = {};
+  const animals = data.species.map((specie) => specie);
+  const locationBruto = animals.map((animal) => animal.location);
+  const locations = locationBruto.filter((este, index) => locationBruto.indexOf(este) === index); // https://pt.stackoverflow.com/questions/16483/remover-elementos-repetido-dentro-de-um-array-em-javascript
+
+  for (let index = 0; index < op.length; index += 1) {
+    if (op[index] !== undefined) {
+      locations.forEach((location) => { objLocation[location] = animalName(location, op); });
+      return objLocation;
+    }
+  }
+  // if (op[0] !== undefined || op[1] !== undefined || op[2] !== undefined || op[3] !== undefined || op[4] !== undefined || op[5] !== undefined) {
+  //     locations.forEach((location) => { objLocation[location] = animalName(location, op); });
+  //     return objLocation;
+  //   }
+  console.log('sai do op for');
+  locations.forEach((location) => { objLocation[location] = animalLocation(location); });
+  return objLocation;
+}
+
+function arrOptions(options) {
+  const arrOptionsAux = [JSON.stringify(options)];
+  console.log(arrOptionsAux);
+  let aux = [];
+  const option = [arrOptionsAux.find((e) => e === '{"includeNames":true}')]; // verificar se tem incluNames
+  aux = arrOptionsAux.find((e) => e === '{"includeNames":true,"sorted":true}'); //
+  option.push(aux);
+  aux = arrOptionsAux.find((e) => e === '{"includeNames":true,"sex":"female"}'); //
+  option.push(aux);
+  aux = arrOptionsAux.find((e) => e === '{"includeNames":true,"sex":"male"}'); //
+  option.push(aux);
+  aux = arrOptionsAux.find((e) => e === '{"includeNames":true,"sex":"female","sorted":true}'); //
+  // console.log(aux);
+  option.push(aux);
+  aux = arrOptionsAux.find((e) => e === '{"includeNames":true,"sex":"male","sorted":true}'); //
+  // console.log(aux);
+  option.push(aux);
+  return option;
+}
+
+function getAnimalMap(options) {
+  // console.log(options);
+  const option = arrOptions(options);
+  if (options === undefined) {
+    return criaObjLocation(option);
+  }
+
+  // console.log(option);
+  if (option[0] === undefined && option[1] === undefined) {
+    // console.log('acessei');
+    return criaObjLocation(option);
+  }
+
+  return criaObjLocation(option);
+}
+// console.log(getAnimalMap({ sex: 'female' }));
+// console.log(animalLocation('NE'));
+// console.log(getAnimalMap({ includeNames: true }));
+// console.log(getAnimalMap({ includeNames: true, sorted: true }));
+// console.log(getAnimalMap({ includeNames: true, sex: 'male' }));
+// console.log(getAnimalMap({ includeNames: true, sex: 'female', sorted: true }));
 module.exports = getAnimalMap;
--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -1,3 +1,45 @@
 const getOpeningHours = require('../src/getOpeningHours');
 
-describe('Testes da função getOpeningHours', () => {});
+describe('Testes da função getOpeningHours', () => {
+  it('retorna um objeto com todos os dias caso nao tenha sido passado um parametro', () => {
+    const actual = getOpeningHours();
+    const expected = {
+      Tuesday: { open: 8, close: 6 },
+      Wednesday: { open: 8, close: 6 },
+      Thursday: { open: 10, close: 8 },
+      Friday: { open: 10, close: 8 },
+      Saturday: { open: 8, close: 10 },
+      Sunday: { open: 8, close: 8 },
+      Monday: { open: 0, close: 0 },
+    };
+    expect(actual).toEqual(expected);
+  });
+  it('retorna aviso caso nao tenha sido passado como parametro os argumentos Monday e 09:00-AM', () => {
+    const actual = getOpeningHours('Monday', '09:00-AM');
+    const expected = 'The zoo is closed';
+    expect(actual).toEqual(expected);
+  });
+  it('retorna aviso caso nao tenha sido passado como parametro os argumentos Wednesday e 09:00-PM', () => {
+    const actual = getOpeningHours('Wednesday', '09:00-PM');
+    const expected = 'The zoo is closed';
+    expect(actual).toEqual(expected);
+  });
+  it('retorna exceção caso nao tenha sido passado como parametro os argumentos Thu e 09:00-AM', () => {
+    expect(() => getOpeningHours('Thu', '09:00-AM')).toThrow(/^The day must be valid. Example: Monday$/);
+  });
+  it('retorna exceção caso nao tenha sido passado como parametro os argumentos Friday e 09:00-ZM', () => {
+    expect(() => getOpeningHours('Friday', '09:00-ZM')).toThrow(/^The abbreviation must be 'AM' or 'PM'$/);
+  });
+  it('retorna exceção caso nao tenha sido passado como parametro os argumentos Saturday e C9:00-AM', () => {
+    expect(() => getOpeningHours('Saturday', 'C9:00-AM')).toThrow(/^The hour should represent a number$/);
+  });
+  it('retorna exceção caso nao tenha sido passado como parametro os argumentos Sunday e 09:c0-AM', () => {
+    expect(() => getOpeningHours('Sunday', '09:c0-AM')).toThrow(/^The minutes should represent a number$/);
+  });
+  it('retorna exceção caso nao tenha sido passado como parametro os argumentos Sunday e 18:00-AM', () => {
+    expect(() => getOpeningHours('Sunday', '18:00-AM')).toThrow(/^The hour must be between 0 and 12$/);
+  });
+  it('retorna exceção caso nao tenha sido passado como parametro os argumentos Sunday e 18:00-AM', () => {
+    expect(() => getOpeningHours('Sunday', '9:60-AM')).toThrow(/^The minutes must be between 0 and 59$/);
+  });
+});
