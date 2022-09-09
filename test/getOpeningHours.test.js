ab220e1d5fc6cb155e0eeac07caaa52ab5a44be1
Ramiro Castro <ramirocastrocavalcanti@outlook.com>
2022-09-09 14:42:20 -0300

    Requisito 13 - getOpeningHours sendo analisado

--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -14,32 +14,63 @@ describe('Testes da função getOpeningHours', () => {
     };
     expect(actual).toEqual(expected);
   });
+
+  const open = 'The zoo is open';
+  const closed = 'The zoo is closed';
+
+  it('retorna aviso caso nao tenha sido passado como parametro os argumentos Thursday e 12:30-PM', () => {
+    const actual = getOpeningHours('Thursday', '12:30-PM');
+    const expected = open;
+    expect(actual).toEqual(expected);
+  });
+
+  it('retorna aviso caso nao tenha sido passado como parametro os argumentos Saturday e 12:00-AM', () => {
+    const actual = getOpeningHours('SaTurday', '12:00-AM');
+    const expected = closed;
+    expect(actual).toEqual(expected);
+  });
+
+  it('retorna aviso caso nao tenha sido passado como parametro os argumentos Sunday e 07:00-PM', () => {
+    const actual = getOpeningHours('Sunday', '07:00-PM');
+    const expected = open;
+    expect(actual).toEqual(expected);
+  });
+
   it('retorna aviso caso nao tenha sido passado como parametro os argumentos Monday e 09:00-AM', () => {
     const actual = getOpeningHours('Monday', '09:00-AM');
-    const expected = 'The zoo is closed';
+    const expected = closed;
     expect(actual).toEqual(expected);
   });
-  it('retorna aviso caso nao tenha sido passado como parametro os argumentos Wednesday e 09:00-PM', () => {
-    const actual = getOpeningHours('Wednesday', '09:00-PM');
-    const expected = 'The zoo is closed';
-    expect(actual).toEqual(expected);
+
+  it('verifica se é função', () => {
+    expect(typeof getOpeningHours).toBe('function');
   });
+
   it('retorna exceção caso nao tenha sido passado como parametro os argumentos Thu e 09:00-AM', () => {
     expect(() => getOpeningHours('Thu', '09:00-AM')).toThrow(/^The day must be valid. Example: Monday$/);
   });
+
   it('retorna exceção caso nao tenha sido passado como parametro os argumentos Friday e 09:00-ZM', () => {
     expect(() => getOpeningHours('Friday', '09:00-ZM')).toThrow(/^The abbreviation must be 'AM' or 'PM'$/);
   });
+
   it('retorna exceção caso nao tenha sido passado como parametro os argumentos Saturday e C9:00-AM', () => {
     expect(() => getOpeningHours('Saturday', 'C9:00-AM')).toThrow(/^The hour should represent a number$/);
   });
+
   it('retorna exceção caso nao tenha sido passado como parametro os argumentos Sunday e 09:c0-AM', () => {
     expect(() => getOpeningHours('Sunday', '09:c0-AM')).toThrow(/^The minutes should represent a number$/);
   });
+
   it('retorna exceção caso nao tenha sido passado como parametro os argumentos Sunday e 18:00-AM', () => {
     expect(() => getOpeningHours('Sunday', '18:00-AM')).toThrow(/^The hour must be between 0 and 12$/);
   });
-  it('retorna exceção caso nao tenha sido passado como parametro os argumentos Sunday e 18:00-AM', () => {
+
+  it('retorna exceção caso nao tenha sido passado como parametro os argumentos Sunday e 9:60-AM', () => {
     expect(() => getOpeningHours('Sunday', '9:60-AM')).toThrow(/^The minutes must be between 0 and 59$/);
+  });
+
+  it('retorna exceção caso nao tenha sido passado como parametro os argumentos SaTurday e 9:60-AM', () => {
+    expect(() => getOpeningHours('SaTurday', '9:60-AM')).toThrow(/^The minutes must be between 0 and 59$/);
   });
 });
